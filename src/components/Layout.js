e7a1a1acae363cd0bab1206e5f38e6761130dca3
mertefe95 <61557140+mertefe95@users.noreply.github.com>
2020-10-01 02:11:32 +0300

    good-one

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,18 @@
-function Layout() {
-    return (
-        
-    )
-  }
-  
-export default Layout;
+import React from 'react'
+import Head from 'next/Head';
+import Zoom from 'react-reveal/Zoom'; 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function Layout({ title, children }) {
+  return (
+    <div>
+        <Head>
+        <script src="https://kit.fontawesome.com/80bd54fff9.js" crossOrigin="anonymous"></script>
+        <title>testing1-2-3</title>
+        </Head>    
+        <main className="container">
+            {children}
+        </main>
+    </div>
+  )
+}
